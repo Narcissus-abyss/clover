e52e7bfa858363ca568af5f15a450b2848ca230e
chensicheng <chensichengmalu@gmail.com>
2021-01-16 02:49:15 +0800

    feat(clover): 添加聊天机器人

---
+++ src/plugins/clover/__init__.py
@@ -0,0 +1,17 @@
+from nonebot import (
+    on_message,
+)
+from nonebot.adapters.cqhttp import GroupMessageEvent, Message
+from nonebot.adapters import Bot
+from .model import (
+    predict
+)
+
+chat = on_message(priority=5)
+
+
+@chat.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    if str(event.group_id) in ["1098933683", "913211173"]:
+        msg: Message = event.get_message()
+        await chat.finish(predict(str(msg)))
