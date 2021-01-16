d930e005539ee81d3b892e1800d3634db495b069
chensicheng <chensichengmalu@gmail.com>
2021-01-16 19:38:14 +0800

    fix: 文字才写入聊天数据文件

--- src/plugins/chat_data/__init__.py
+++ src/plugins/chat_data/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import (
     on_message,
 )
-from nonebot.adapters.cqhttp import GroupMessageEvent, Message
+from nonebot.adapters.cqhttp import GroupMessageEvent, MessageSegment, Message
 from nonebot.adapters import Bot
 from aredis import StrictRedis
 
@@ -13,11 +13,13 @@ save_chat_data = on_message(priority=5, block=False)
 
 @save_chat_data.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
-    msg = str(event.get_message())
-    group_id = str(event.group_id)
-    with open(f'/var/www/{group_id}_chat.log', 'a+') as file:
-        if not await redis_client.exists(group_id):
-            await redis_client.set(group_id, True)
-            await redis_client.expire(group_id, 60)
-            file.write("\n")
-        file.write(f"{msg}\n")
+    msg: Message = event.get_message()
+    for item in msg:
+        if item.type == 'text':
+            group_id = str(event.group_id)
+            with open(f'/var/www/chat_data/{group_id}_chat.log', 'a+') as file:
+                if not await redis_client.exists(group_id):
+                    await redis_client.set(group_id, True)
+                    await redis_client.expire(group_id, 60)
+                    file.write("\n")
+                file.write(f"{str(msg)}\n")
