096ac3ddcbe0f82a42521a7eff5be6423ee1bd47
chensicheng <chensichengmalu@gmail.com>
2021-01-16 13:54:43 +0800

    fix: 添加聊天响应群号

--- src/plugins/clover/__init__.py
+++ src/plugins/clover/__init__.py
@@ -12,6 +12,6 @@ chat = on_message(priority=5)
 
 @chat.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
-    if str(event.group_id) in ["1098933683", "913211173"]:
+    if str(event.group_id) in ["1098933683", "913211173", "993211173"]:
         msg: Message = event.get_message()
         await chat.finish(predict(str(msg)))
