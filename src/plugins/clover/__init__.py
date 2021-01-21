97d93f4e3aefb6d436760b56329160bdbe3c123e
chensicheng <chensichengmalu@gmail.com>
2021-01-21 22:56:51 +0800

    fix: 添加测试群响应clover

--- src/plugins/clover/__init__.py
+++ src/plugins/clover/__init__.py
@@ -12,6 +12,6 @@ chat = on_message(priority=5, block=False)
 
 @chat.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
-    if str(event.group_id) in ["1098933683", "913211173", "993211173"]:
+    if str(event.group_id) in ["1098933683", "913211173", "993211173", "1124718762"]:
         msg: Message = event.get_message()
         await chat.finish(predict(str(msg)))
